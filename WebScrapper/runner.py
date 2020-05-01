fc748fe043849fee8f5005d6ab26ba1593a1b8c1
Greg K <gpkort@gmail.com>
2020-04-30 22:08:08 -0600

    re commit

---
+++ WebScrapper/runner.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+# page = requests.get(URL)
+#
+
+
+#  Allen Ginsberg, Lawrence Ferlinghetti, Gregory Corso, and Gary Snyder
+# http://famouspoetsandpoems.com/poets/allen_ginsberg/poems
+# http://famouspoetsandpoems.com/poets/lawrence_ferlinghetti/poems
+# http://famouspoetsandpoems.com/poets/gregory_corso/poems
+# http://famouspoetsandpoems.com/poets/gary_snyder/poems
+POETS = ["allen_ginsberg", "lawrence_ferlinghetti", "gregory_corso", "gary_snyder"]
+URL_PREFIX = "http://famouspoetsandpoems.com/poets/"
+URL_POST = "/poems"
+
+current_url = f"{URL_PREFIX}{POETS[0]}{URL_POST}"
+print(current_url)
+
+page = requests.get(current_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+for link in soup.find_all('a'):
+    href = link.get('href')
+    poem_href = f'/poets/{POETS[0]}/poems'
+    if poem_href in href and href != poem_href:
+        print(href)
+
